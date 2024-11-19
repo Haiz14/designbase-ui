64b97a3fd299e3caa4083797fc11d7229f328250
Haiz14 <haiz.gigs@gmail.com>
2024-11-19 22:18:13 +0530

    feat: added main site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	const content = {
+	  "quote": "Baby Steps, Big Results",
+		"description": "Starting from simple content based components like hero section, headers and features to complex dashboard, from simple design with only spacing to slick-animation, colors and typography. from simple content in header like branding only to headers crammed with information. This designbase-ui shows you all, explains it all. ",
+	};
+</script>
+
+<div class="my-20 mx-4 space-y-10">
+	<div class="space-y-6">
+		<h1 class="text-5xl"> {content.quote} </h1>
+		<h4 class="text-2xl leading-10"> {content.description} </h4>
+  </div>
+</div>
